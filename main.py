9ed5540dd7b1244162ab7e8cd43e5c396e133a7c
Ryu_Ronin <creative.ryu@web.de>
2023-03-27 16:14:03 +0200

    covert image load and placing surfaces

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import pygame, sys
+import pygame
+import sys
 
 pygame.init()
 SCREEN_SIZE = 1280, 720
 display_surface = pygame.display.set_mode((SCREEN_SIZE))
 pygame.display.set_caption("Asteroid Shooter")
+
+# importing images
+ship_surface = pygame.image.load("ressources/graphics/ship.png").convert_alpha()
+background = pygame.image.load("ressources/graphics/background.png").convert()
+
 
 while True:
     # 1. Player inputs -> inputs sind events
@@ -11,8 +17,11 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
-        
+
     # 2. Updates
-    # nothing yet
+    display_surface.fill((100, 100, 100))
+    display_surface.blit(background, (0, 0))
+    display_surface.blit(ship_surface, (300, 500))
+
     # 3. show Frame to Player -> update display_surface
     pygame.display.update()
